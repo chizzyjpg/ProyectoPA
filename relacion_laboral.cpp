c331f397420ca4789783f5e8aff0064d0c9ed2ce
chizzyjpg <64444067+chizzyjpg@users.noreply.github.com>
2024-03-25 16:09:09 -0300

    Update relacion_laboral.cpp

--- relacion_laboral.cpp
+++ relacion_laboral.cpp
@@ -9,26 +9,31 @@ RelacionLaboral::RelacionLaboral(float Sue, Empresa *Empre){
 
 void RelacionLaboral::setSueldo(float Sue){
     this->sueldo = Sue;
-}
+};
 void RelacionLaboral::setFechaDesvinculacion(Fecha *Fech){
     this->fechaDesvinculacion = Fech;
-}
+};
 void RelacionLaboral::setLaEmpre(Empresa *Empre){
     this->LaEmpre = Empre;
-}
+};
 float RelacionLaboral::getSueldo(){
     return this->sueldo;
-}
+};
 Fecha* RelacionLaboral::getFechaDesvinculacion(){
     return this->fechaDesvinculacion;
-}
-Empresa* getLaEmpre(){
+};
+Empresa* RelacionLaboral::getLaEmpre(){
     return this->LaEmpre;
-}
+};
 float RelacionLaboral::getSueldoLiquido (){
-    return (this->getSueldo()*this->LaEmpre->getDescuento())//Algo asi?
-}
+
+    /*float descuento;
+    descuento = (this->getSueldo()*this->LaEmpre->getDescuento()/100)
+    return (this->getSueldo() - descuento)*/
+
+    /*return (this->getSueldo()*this->LaEmpre->getDescuento());   Algo asi?*/
+};
 
 RelacionLaboral::~RelacionLaboral(){
     
-}
+};
