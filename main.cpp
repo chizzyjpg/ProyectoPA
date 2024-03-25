7d8ce5e83c5f08d4acbef7db6676beaae77f251f
Fabricio <fuentesfabricio17@gmail.com>
2024-03-25 17:31:51 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+int main(){
+
+}
+
+/*
+a) void agregarEmpleado(string ci, string nombre, string apellido, Direccion dir) 
+Crea un nuevo empleado en el sistema.
+En caso de ya existir, levanta la excepción std::invalid_argument.
+
+b) void agregarEmpresa(DtEmpresa * empresa), 
+Crea una nueva empresa en el sistema. 
+En caso de ya existir, levanta una excepción std::invalid_argument.
+
+c) DtEmpleado** listarEmpleados(int & cantEmpleados)
+Retorna un arreglo de DtEmpleado* con todos los empleados del sistema. 
+El largo del arreglo de empleados está dado por el parámetro cantEmpleados.
+
+d) void agregarRelacionLaboral(String ciEmpleado, string idEmpresa, float sueldo)
+Vincula un empleado con una empresa. 
+Si la empresa ya está dentro de las empresas que el empleado ha trabajado o trabaja se levanta una excepción std::invalid_argument.
+
+e) void finalizarRelacionLaboral(string ciEmpleado, string idEmpresa, Fecha desvinculación)
+Desvincula al empleado de la empresa, registrando la fecha en que terminó el vinculo.
+
+f) DtEmpresa** obtenerInfoEmpresaPorEmpleado(string ciEmpleado, int & cantEmpresas)
+Retorna un arreglo con las empresas donde trabaja activamente el empleado. 
+El largo del arreglo de empresas está dado por el parámetro cantEmpresas.
+
+
+Nota: A los efectos de este laboratorio, la función main mantendrá una colección de
+empleados, implementada como un arreglo de tamaño MAX_EMPLEADOS. 
+Lo mismo para las empresas del sistema con un máximo de MAX_EMPRESAS (ambas constantes). 
+Puede implementar operaciones en las clases dadas en el modelo si
+considera que le facilitan para la resolución de las operaciones pedidas en el main.
+*/
